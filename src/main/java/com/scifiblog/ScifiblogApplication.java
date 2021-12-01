bf8783f6e3b150582ad198500e5fa5b21216bcbb
Geovani Ferreira <gugaheadbanger@gmail.com>
2021-12-01 19:35:18 -0300

    Estrutura inicial do scifiblog com a entidade postagem

---
+++ src/main/java/com/scifiblog/ScifiblogApplication.java
@@ -0,0 +1,13 @@
+package com.scifiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScifiblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScifiblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scifiblog/ScifiblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scifiblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScifiblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
